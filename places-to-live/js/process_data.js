ddd9c13753f5c9ac55f2fd647c324316d6b99e71
Mohan <mohan.kandaraj@gmail.com>
2016-05-11 23:37:59 -0400

    Added Left and right panel- p1

--- places-to-live/js/process_data.js
+++ places-to-live/js/process_data.js
@@ -1,6 +1,11 @@
 (function() {
     /*global d3 google topojson */
     var places = {};
+    var col_names = [];
+    var score = {};
+    var county_name = {};
+    var state_name = {};
+    // var counties_map = {};
 
     d3.csv("./data/place_score.csv",
         function(data) {
@@ -23,7 +28,32 @@
     function process_data(data) {
         draw_map(data);
         places = data;
+        for (variable in data[0]) {
+            col_names.push({
+                label: variable,
+                value: variable
+            });
+        }
+        // console.log(col_names);
+        $("#vars").multiselect({
+            delimiterText: "<br>",
+            enableHTML: true,
+            numberDisplayed: 7,
+            onChange: function() {
+                limit_selection("#vars", 7, 3);
+            }
+        });
+
+        //Add the variable as options skip first 4 variables as they are
+        //        for FIPS, County, State and Overall_score
+        var var_opts = col_names.slice(4);
+        $("#vars").multiselect("dataprovider", var_opts);
+        $("#vars").multiselect("select", var_opts.slice(0, 5).map(function(d) {
+            return d.label;
+        }));
         // console.log(data[1]);
+        add_sliders();
+
         google.charts.load("current", {
             "packages": ["table", "corechart", "bar"]
         });
@@ -33,9 +63,10 @@
     function draw_map(place_score) {
         /*eslint-env d3, jquery*/
         /*global d3*/
-        var width = $("#map").width() *.9;
+        var width = $("#map").width();
         var height = $("#map").height();
-        console.log(height);
+        var init_scale = width / 950;
+        // console.log(height);
 
         // Setting color domains(intervals of values) for our map
         var color_domain = [50, 100, 150, 200, 250, 300, 350, 400, 450];
@@ -50,7 +81,7 @@
             .style("opacity", 0);
 
         var svg = d3.select("#map").append("svg")
-            .attr("width", "90%")
+            .attr("width", "100%")
             .attr("height", "100%")
             .style("margin", "10px auto");
         //console.log(svg);
@@ -59,15 +90,14 @@
 
         //Reading map file and data
 
-        d3.json("data/county.json",county_map);
+        d3.json("data/county.json", county_map);
 
         //Start of Choropleth drawing
 
         function county_map(error, counties) {
             //  console.log(counties);
-            var score = {};
-            var county_name = {};
-            var state_name = {};
+
+            // counties_map = counties;
 
             place_score.forEach(function(d) {
                 score[+d.FIPS] = +d.Overall_Score;
@@ -77,8 +107,7 @@
             //console.log(score);
             //Drawing Choropleth
             var g = svg.append("g").attr("class", "region")
-                .attr("id", "reg_1")
-                .attr("transform", "scale(1.2)");
+                .attr("id", "reg_1");
 
             //g.append("g")
             //.attr("class", "region")
@@ -94,6 +123,7 @@
                 .style("opacity", 0.8)
                 //Adding mouseevents
                 .on("mouseover", function(d) {
+                    // d3.select(this).moveToFront();
                     div.transition().duration(300)
                         .style("opacity", 1);
                     div.html(state_name[d.id] + " - " + county_name[d.id] + "<br> Score : " + score[d.id])
@@ -104,6 +134,7 @@
                     div.transition().duration(300)
                         .style("opacity", 0);
                 });
+
             //Adding legend for our map
             var legend = svg.selectAll("g.legend")
                 .data(ext_color_domain)
@@ -135,18 +166,23 @@
                 });
             //Define Zoom behavior
             var zoom = d3.behavior.zoom()
-                .scaleExtent([1.2, 16])
-                .scale(0.5)
+                .scaleExtent([0.5, 16])
+                .scale(init_scale)
                 .on("zoom", zoomed);
 
             //Set zoom behavior for svg
             svg.call(zoom).call(zoomed);
+            // g.attr("transform","scale("+init_scale+")");
 
             //zoom function
             function zoomed() {
-                var g = d3.select("#reg_1");
-                g.attr("transform",
-                    "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+                // var g = d3.select("#reg_1");
+                // g.attr("transform",
+                //     "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+                d3.select("#reg_1")
+                    .attr("transform",
+                        "translate(" + zoom.translate() + ")scale(" + zoom.scale() + ")");
+
                 //console.log(d3.event.scale);
             }
 
@@ -156,7 +192,6 @@
 
 
     }
-
 
 
 
@@ -236,5 +271,79 @@
         chart.draw(data2, options);
 
     }
+
+    function limit_selection(id, max, min) {
+        var selectedOptions = $(id).find(":selected");
+
+        if (selectedOptions.length >= max) {
+            // Disable all other checkboxes.
+            var nonSelectedOptions = $(id).find("option").filter(function() {
+                return !$(this).is(":selected");
+            });
+
+            // var dropdown = $(id).siblings(".multiselect-container");
+            nonSelectedOptions.each(function() {
+                var input = $("input[value='" + $(this).val() + "']");
+                input.prop("disabled", true);
+                input.parent("li").addClass("disabled");
+            });
+        } else if (selectedOptions.length <= min) {
+            // var dropdown = $(id).siblings(".multiselect-container");
+            selectedOptions.each(function() {
+                var input = $("input[value='" + $(this).val() + "']");
+                input.prop("disabled", true);
+                input.parent("li").addClass("disabled");
+            });
+        } else {
+            // Enable all checkboxes.
+            // var dropdown = $(id).siblings(".multiselect-container");
+            $(id).find("option").each(function() {
+                var input = $("input[value='" + $(this).val() + "']");
+                input.prop("disabled", false);
+                input.parent("li").addClass("disabled");
+            });
+            // d3.selectAll("path")
+            //     .style("fill", function(d,i) {
+            //         if (i==1) console.log(d);
+            //         return ["white","orange"][i%3];
+            //     });
+        }
+    }
+
+
+        function add_sliders() {
+
+            // $("#wt1").on("change", wt_change);
+            // wt_change();
+
+            var vars_selected = $("#vars").find(":selected");
+            // console.log(vars_selected);
+            for (var i = 1; i <= 5; i++) {
+                $("#wt" + i).slider();
+                $("#wt" + i).on("change", wt_change);
+                $("#wt" + i + "_label").text(vars_selected[i - 1].label + " - Wt: " + $("#wt" + i).val());
+                // console.log("wt"+i+"_label"+vars_selected[i-1].label+" - Wt: " + "#wt"+i);
+
+            }
+
+            function wt_change() {
+                var id = this.id;
+                // console.log("change");
+                // console.log($("#vars").find(":selected")[0].label);
+                $("#" + id + "_label").text($("#vars").find(":selected")[id.slice(-1) - 1].label + "- Wt: " + $(this).val());
+                var color_domain = [50, 100, 150, 200, 250, 300, 350, 400, 450];
+                var color = d3.scale.threshold()
+                    .domain(color_domain)
+                    .range(["#dcdcdc", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#5e8b73", "#387255", "#256546", "#004d28"]);
+                var factor = $(this).val();
+                d3.selectAll("path")
+                    .style("fill", function(d) {
+                        // console.log(color(score[+d.id] * factor/10));
+                        return color(score[+d.id] * factor/10);
+                    });
+
+            }
+        }
+
 
 })(this);
