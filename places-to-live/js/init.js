2a7e0abb5df96f8cabbc7bdd5edaf125f8db7ea2
Mohan <mohan.kandaraj@gmail.com>
2016-05-17 18:00:53 -0400

    Added Documentation

--- places-to-live/js/init.js
+++ places-to-live/js/init.js
@@ -1,27 +1,44 @@
 $(document).ready(function() {
     //Script to initialize UI and set Inital Dispplay
-    $("#map_sel,#data_sel,#nj_sel").click(function() {
+    $("#map_sel,#data_sel,#nj_sel,#doc_sel").click(function() {
         $(this).addClass("active").siblings().removeClass("active");
         switch($(this).text()) {
             case "Nation":
                 $("#map").css("display", "block");
                 $("#dt1").css("display", "none");
                 $("#dt2").css("display", "none");
+                $("#doc").css("display", "none");
                 $("#right_panel").css("display", "block");
+                $("#left_panel").css("visibility", "visible");
                 $("#top_dest").css("display", "block");
                 break;
             case "State" :
                 $("#map").css("display", "none");
+                $("#doc").css("display", "none");
                 $("#dt2").css("display", "block");
                 $("#dt1").css("display", "none");
+                $("#left_panel").css("visibility", "visible");
                 $("#right_panel").css("display", "block");
                 $("#top_dest").css("display", "none");
                 break;
             case "Data" :
+                $("#left_panel").css("display", "block");
                 $("#map").css("display", "none");
+                $("#doc").css("display", "none");
                 $("#dt2").css("display", "none");
                 $("#dt1").css("display", "block");
+                $("#left_panel").css("display", "block");
+                $("#right_panel").css("visibility", "visible");
+                break;
+            case "Doc":
+                $("#map").css("display", "none");
+                $("#dt2").css("display", "none");
+                $("#dt1").css("display", "none");
+                $("#doc").css("display", "block");
                 $("#right_panel").css("display", "none");
+                $("#left_panel").css("visibility", "hidden");
+
+
 
         }
 
